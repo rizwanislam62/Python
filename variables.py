ddac461eff994e136ad6d7a972fb5f968548f907
rizwan <rizwan@localhost.localdomain>
2025-07-28 16:43:25 +0500

    learn about different variable types

---
+++ variables.py
@@ -0,0 +1,10 @@
+var=10 #int
+float=2.2#float
+isbool=True #boolean value either true or false 
+Stringg='rizwan' #string variable type 
+print(var)
+print(float)
+print(isbool)
+print(Stringg)
+
+print(type(var), type(float),type(isbool), type(Stringg))
