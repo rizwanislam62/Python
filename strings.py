90c552f47143877ded77ff3d8c0b00a66b5c4dbe
rizwan <rizwan@localhost.localdomain>
2025-07-29 22:16:34 +0500

    added string and also find the index

---
+++ strings.py
@@ -0,0 +1,10 @@
+string="what is your name ?"
+print(string[-1])# the -1 shows the negative index of string 
+
+
+str1=''' 
+my name is rizwan 
+i am network engineer
+
+'''
+print(str1) #the three commas print the line as it is 
