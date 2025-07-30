0c7dba41ab32d46ebbbd0a5d0ed31e77bc518b94
rizwan <rizwan@localhost.localdomain>
2025-07-30 07:54:32 +0500

    print the characters of different ranges

--- strings.py
+++ strings.py
@@ -1,6 +1,5 @@
 string="what is your name ?"
-print(string[-1])# the -1 shows the negative index of string 
-
+print(string[:10])# the -1 shows the negative index of string 
 
 str1=''' 
 my name is rizwan 
