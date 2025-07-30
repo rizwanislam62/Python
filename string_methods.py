019a3cd47c0a480b8c9c28d31a63a425e1482375
rizwan <rizwan@localhost.localdomain>
2025-07-30 09:20:00 +0500

    different methods applied to string,upper,lower, length, replace

---
+++ string_methods.py
@@ -0,0 +1,10 @@
+course="python for new learners"
+print(len(course)) #print the length of string
+print(course.upper()) # convert into upper case
+print(course.lower()) # print into lower case
+print(course.__len__()) #print the  length
+
+
+print(course.replace("new learners", "Absolute beginners")) # replace the string
+
+print(course.find("o")) # find the character  in string 
