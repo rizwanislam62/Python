31be65a0b02e8b77a9ef6dffb4453c5be3ef31b9
rizwanislam62 <rizwanislam62@gmail.com>
2025-07-30 11:40:36 +0500

    conditional operator

---
+++ conditional operators.py
@@ -0,0 +1,15 @@
+Marks=90
+Grade ="A" if Marks>= 90 else "B"
+print(Grade)
+
+
+# conditional operator, if one of the condition is true in case of or operator it will make it true 
+# in case of and if both are true or false  then it will give output 
+
+criminal_record=True
+good_credit=False
+
+if criminal_record and good_credit:
+    print("eligible")
+else:
+    print("not eligible")
